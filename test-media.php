5c83e81d56e88b51c5995f987a78717112847534
Ravi Gaudani <ravigaudani.ibl@gmail.com>
2018-07-10 16:18:05 +0530

    first commit

---
+++ test-media.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <video poster="" playsinline="" autoplay muted loop preload="auto">
+        <source src="https://socialkids.com/kidsactivity/assets/video/Clay-HD.webm" type="video/webm">
+        <source src="https://socialkids.com/kidsactivity/assets/video/Clay-HD.mp4" type="video/mp4">
+        <source src="https://socialkids.com/kidsactivity/assets/video/Clay-HD.ogg" type="video/ogg">
+    </video>
+</body>
+</html>
